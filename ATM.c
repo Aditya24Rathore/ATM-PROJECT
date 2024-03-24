2215ac2bb7c1a40f4fa22ef5e023aaffb7a5b89e
Aditya Rathore <rathoreaditya262@gmail.com>
2024-03-24 07:26:10 +0530

    Adding the PIN feature

--- ATM.c
+++ ATM.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 float withdraw,deposit,amt=0;
+int genpin,checkPin;
+
+int PIN(){
+    printf("\n\nenter the PIN : \n\n");
+    scanf("%d",&checkPin);
+    return checkPin;
+}
+
+int GPIN(){
+    printf("\n\ngenerate ATM Pin number : \n\n");
+    scanf("%d",&genpin);
+    return genpin;
+}
 void CheckBalance(){
     printf("\nCURRENR BALANCE IS : %.2fRs\n\n",amt);
 }
@@ -23,9 +37,23 @@ void Withdraw(){
 }
 
 int main(){
+    GPIN();
+    system("cls");
+    PIN();
+    
+    while(genpin!=checkPin){
+        PIN();
+
+        if(genpin==checkPin){
+            break;
+        }
+        else{
+            printf("\n\ninvlid PIN\n\n");
+        }   
+    }     
  int ch;
  do {
-    printf(" 1 : CHECK BALANCE \n 2 : DEPOSIT \n 3 : WITHDRAW \n 0 : CANCEL TRANSATION \n");
+    printf("\n\n 1 : CHECK BALANCE \n 2 : DEPOSIT \n 3 : WITHDRAW \n 0 : CANCEL TRANSATION \n");
     printf("\nenter Choice : ");
     scanf("%d",&ch);
         switch(ch){
@@ -44,5 +72,6 @@ int main(){
             default : printf("\n\n**invalid choice**\n\n");    
         }
     } while (ch!=0);
+        
   return 0;
 }
